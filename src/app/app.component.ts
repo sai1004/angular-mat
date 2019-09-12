3aa01752ca8a0e8ba39848ffc5e312bd07c747e5
sai1004 <saikiran.bavandla@gmail.com>
2019-09-12 19:25:07 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import * as AOS from "aos";
+import 'aos/dist/aos.css';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'angular-mat';
+  isShowHeadActive: boolean;
+  ngOnInit() {
+      AOS.init();
+  }
+  constructor(private router: Router) { }
+  title = "Tour of Heroes";
+
+  onDeactivate() {
+      // document.body.scrollTop = 0;
+      // Alternatively, you can scroll to top by using this other call:
+      window.scrollTo(0, 0)
+  }
+
 }
