4e38c427de1e7f737a4f5cd89a0b598643c9acb0
sai1004 <saikiran.bavandla@gmail.com>
2019-10-01 19:10:32 +0530

    commit

--- src/app/components/maxit-design/maxit-design.component.ts
+++ src/app/components/maxit-design/maxit-design.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./maxit-design.component.css']
 })
 export class MaxitDesignComponent implements OnInit {
+  
 
+  email: string = "Aman@gmail.com";
+  updateName(input: string) {
+      this.email = input;
+  }
   constructor() { }
 
   ngOnInit() {
