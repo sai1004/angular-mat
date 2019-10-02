b67b4ee6906ee816a706ba80ac82197d1ecef2a7
sai1004 <saikiran.bavandla@gmail.com>
2019-10-02 22:03:06 +0530

    commit

--- src/app/components/maxit-banner/maxit-banner.component.ts
+++ src/app/components/maxit-banner/maxit-banner.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-maxit-banner',
-  templateUrl: './maxit-banner.component.html',
-  styleUrls: ['./maxit-banner.component.css']
+  selector: "app-maxit-banner",
+  templateUrl: "./maxit-banner.component.html",
+  styleUrls: ["./maxit-banner.component.css"]
 })
 export class MaxitBannerComponent implements OnInit {
+  items: any[] = [
+    {
+      img: "../../../assets/img/icon/f-icon-3.png"
+    },
+    {
+      img: "../../../assets/img/icon/f-icon-3.png"
+    },
+    {
+      img: "../../../assets/img/icon/f-icon-3.png"
+    },
+    {
+      img: "../../../assets/img/icon/f-icon-3.png"
+    },
+    {
+      img: "../../../assets/img/icon/f-icon-3.png"
+    }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
