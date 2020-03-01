5d3dda0df8f50f7f30bdabc0e4a32a2e43d9b183
sai1004 <saikiran.bavandla@gmail.com>
2020-03-01 19:42:11 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,30 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
 import { LayoutModule } from "@angular/cdk/layout";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { AppRoutingModule } from "./app-routing.module";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { SharedModule } from "./shared/shared.module";
-import { NavComponent } from './components/nav/nav.component';
-import { WelcomeComponent } from './pages/welcome/welcome.component';
-import { BannerComponent } from './components/banner/banner.component';
-import { MaxitLandingPageComponent } from './pages/maxit-landing-page/maxit-landing-page.component';
-import { MaxitBannerComponent } from './components/maxit-banner/maxit-banner.component';
-import { MaxitNavComponent } from './components/maxit-nav/maxit-nav.component';
-import { MaxitFooterComponent } from './components/maxit-footer/maxit-footer.component';
-import { MaxitDesignComponent } from './components/maxit-design/maxit-design.component';
-import { MaxitClientOfferComponent } from './components/maxit-client-offer/maxit-client-offer.component';
-import { MaxitCarouselComponent } from './components/maxit-carousel/maxit-carousel.component';
-import { MaxitPremiumComponent } from './components/maxit-premium/maxit-premium.component';
-import { MaxitRecentBlogComponent } from './components/maxit-recent-blog/maxit-recent-blog.component';
-import { MaxitProjectsComponent } from './components/maxit-projects/maxit-projects.component';
-import { MaxitAboutPageComponent } from './pages/maxit-about-page/maxit-about-page.component';
-import { MaxitTestComponent } from './components/maxit-test/maxit-test.component';
-import { MaxitTestRightComponent } from './components/maxit-test-right/maxit-test-right.component';
+import { NavComponent } from "./components/nav/nav.component";
+import { WelcomeComponent } from "./pages/welcome/welcome.component";
+import { BannerComponent } from "./components/banner/banner.component";
+import { MaxitLandingPageComponent } from "./pages/maxit-landing-page/maxit-landing-page.component";
+import { MaxitBannerComponent } from "./components/maxit-banner/maxit-banner.component";
+import { MaxitNavComponent } from "./components/maxit-nav/maxit-nav.component";
+import { MaxitFooterComponent } from "./components/maxit-footer/maxit-footer.component";
+import { MaxitDesignComponent } from "./components/maxit-design/maxit-design.component";
+import { MaxitClientOfferComponent } from "./components/maxit-client-offer/maxit-client-offer.component";
+import { MaxitCarouselComponent } from "./components/maxit-carousel/maxit-carousel.component";
+import { MaxitPremiumComponent } from "./components/maxit-premium/maxit-premium.component";
+import { MaxitRecentBlogComponent } from "./components/maxit-recent-blog/maxit-recent-blog.component";
+import { MaxitProjectsComponent } from "./components/maxit-projects/maxit-projects.component";
+import { MaxitAboutPageComponent } from "./pages/maxit-about-page/maxit-about-page.component";
+ 
 
 // import { CartItemService } from './components/cart-item.service';
- 
 
 @NgModule({
   declarations: [
@@ -47,8 +45,6 @@ import { MaxitTestRightComponent } from './components/maxit-test-right/maxit-tes
     MaxitRecentBlogComponent,
     MaxitProjectsComponent,
     MaxitAboutPageComponent,
-    MaxitTestComponent,
-    MaxitTestRightComponent
  
   ],
   imports: [
@@ -62,9 +58,7 @@ import { MaxitTestRightComponent } from './components/maxit-test-right/maxit-tes
     HttpClientModule
   ],
 
-  providers: [
-   
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
